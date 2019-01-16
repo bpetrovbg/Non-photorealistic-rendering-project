6a32faa5b2032070a9b1c3cc68c2c122937b38fc
bpetrovbg <bozhidarpetrovbg@gmail.com>
2019-01-16 13:35:04 +0200

    Add project files.

---
+++ NPR/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace NPR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.floydSteinbergToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(845, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.floydSteinbergToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // floydSteinbergToolStripMenuItem
+            // 
+            this.floydSteinbergToolStripMenuItem.Name = "floydSteinbergToolStripMenuItem";
+            this.floydSteinbergToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.floydSteinbergToolStripMenuItem.Text = "Floyd-Steinberg";
+            this.floydSteinbergToolStripMenuItem.Click += new System.EventHandler(this.floydSteinbergToolStripMenuItem_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(9, 25);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(825, 469);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picOriginal_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picOriginal_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 505);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "NPR Floyd-Steinberg";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem floydSteinbergToolStripMenuItem;
+    }
+}
+
---
+++ NPR/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NPR
+{
+    public partial class Form1 : Form
+    {
+        // The original image.
+        private Bitmap OriginalImage;
+        private Bitmap outBm;
+        private Bitmap area;
+
+        // True when we're selecting a rectangle.
+        private bool IsSelecting = false;
+
+        // The area we are selecting.
+        private int X0, Y0, X1, Y1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                OriginalImage = (Bitmap)Image.FromFile(openFileDialog.FileName);
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox.Image = OriginalImage;
+            }
+        }
+
+        private void floydSteinbergToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int r, r1, r2, r3, r4;
+            int error;
+            int s;
+            if (area == null)
+            {
+                MessageBox.Show("No image loaded, please select File -> Load Image, then try again.", "No image loaded", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (area.Width > area.Height)
+                s = area.Height;
+            else
+                s = area.Width;
+            outBm = new Bitmap(s, s);
+
+            for (int i = s - 2; i > 0; i--)
+                for (int j = 1; j < s - 1; j++)
+                {
+                    int c = area.GetPixel(i, j).R;
+                    r = Approximate(c);
+                    outBm.SetPixel(i, j, Color.FromArgb(r, r, r));
+                    error = c - r;
+                    r1 = area.GetPixel(i, j + 1).R + 7 * error / 16;
+                    r2 = area.GetPixel(i + 1, j - 1).R + 3 * error / 16;
+                    r3 = area.GetPixel(i + 1, j).R + 5 * error / 16;
+                    r4 = area.GetPixel(i + 1, j + 1).R + error / 16;
+
+                    if (r1 >= 255) r1 = 255;
+                    if (r2 >= 255) r2 = 255;
+                    if (r3 >= 255) r3 = 255;
+                    if (r4 >= 255) r4 = 255;
+                    outBm.SetPixel(i, j + 1, Color.FromArgb(r1, r1, r1));
+                    outBm.SetPixel(i + 1, j - 1, Color.FromArgb(r2, r2, r2));
+                    outBm.SetPixel(i + 1, j, Color.FromArgb(r3, r3, r3));
+                    outBm.SetPixel(i + 1, j + 1, Color.FromArgb(r4, r4, r4));
+
+                }
+            pictureBox.Image = outBm;
+
+        }
+    
+
+        private void picOriginal_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (OriginalImage == null) return;
+            IsSelecting = true;
+
+            // Save the start point.
+            X0 = e.X;
+            Y0 = e.Y;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {   if (OriginalImage == null) return;
+            // Do nothing it we're not selecting an area.
+            if (!IsSelecting) return;
+            IsSelecting = false;
+
+            // Copy the selected part of the image.
+            int wid = Math.Abs(X0 - X1);
+            int hgt = Math.Abs(Y0 - Y1);
+            if ((wid < 1) || (hgt < 1)) return;
+
+            area = new Bitmap(wid, hgt);
+            using (Graphics gr = Graphics.FromImage(area))
+            {
+                Rectangle source_rectangle =
+                    new Rectangle(Math.Min(X0, X1), Math.Min(Y0, Y1),
+                        wid, hgt);
+                Rectangle dest_rectangle =
+                    new Rectangle(0, 0, wid, hgt);
+                gr.DrawImage(OriginalImage, dest_rectangle,
+                    source_rectangle, GraphicsUnit.Pixel);
+            }
+
+        }
+
+        private int Approximate(int c)
+        {
+            if (c >= 0 && c < 63)
+                return 0;
+            if (c >= 63 && c < 127)
+                return 63;
+            if (c >= 127 && c < 191)
+                return 127;
+            if (c >= 191 && c < 255)
+                return 191;
+            else
+                return 0;
+        }
+
+        private void picOriginal_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (OriginalImage == null) return;
+            // Do nothing it we're not selecting an area.
+            if (!IsSelecting) return;
+
+            // Save the new point.
+            X1 = e.X;
+            Y1 = e.Y;
+
+            // Make a Bitmap to display the selection rectangle.
+            Bitmap bm = new Bitmap(OriginalImage);
+
+            // Draw the rectangle.
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.DrawRectangle(Pens.Red,
+                    Math.Min(X0, X1), Math.Min(Y0, Y1),
+                    Math.Abs(X0 - X1), Math.Abs(Y0 - Y1));
+            }
+
+            // Display the temporary bitmap.
+            pictureBox.Image = bm;
+        }
+    }
+}
